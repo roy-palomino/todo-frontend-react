dddab0732775de4f79cb27c51286efc75c6f7377
Roy Palomino Rojas <roy100995@gmail.com>
2024-07-25 16:57:53 -0500

    feat: add badge to display tag with custom styles

---
+++ src/components/TagBadge.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+
+import { Tag } from "../models";
+import { classNames } from "../utils/classNames";
+
+interface Props extends Tag {}
+
+const MyComponent: FC<Props> = (tag) => {
+  return (
+    <span
+      className={classNames(
+        "border rounded-md px-3 py-1 font-semibold text-xs border-opacity-15",
+        "text-" + tag.text_color,
+        "bg-" + tag.background_color,
+        "border-" + tag.text_color,
+      )}
+    >
+      {tag.name}
+    </span>
+  );
+};
+
+export default MyComponent;
