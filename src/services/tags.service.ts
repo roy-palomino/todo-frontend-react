78d54321660af27a157de918d2209543452980ef
Roy Palomino Rojas <roy100995@gmail.com>
2024-07-24 20:09:38 -0500

    feat: add service to create tags

--- src/services/tags.service.ts
+++ src/services/tags.service.ts
@@ -1,11 +1,25 @@
 import axios from "axios";
 
 import { API_BASE_URL, LIST_TAGS } from "../shared/constants";
-import { Tag } from "../models";
+import { Tag, CreateTagPayload } from "../models";
 
 export async function listTags(): Promise<Tag[]> {
   try {
     const result = await axios.get<Tag[]>(`${API_BASE_URL}${LIST_TAGS}`);
+    return result.data;
+  } catch (e: any) {
+    throw new Error(e);
+  }
+}
+
+export async function createTag(tag: CreateTagPayload): Promise<Tag> {
+  const access = localStorage.getItem("access");
+  try {
+    const result = await axios.post(`${API_BASE_URL}${LIST_TAGS}`, tag, {
+      headers: {
+        Authorization: `Bearer ${access}`,
+      },
+    });
     return result.data;
   } catch (e: any) {
     throw new Error(e);
