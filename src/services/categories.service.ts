aeeb042b9878e86c811f496a524e112f3cb4b0fe
Roy Palomino Rojas <roy100995@gmail.com>
2024-07-24 20:09:19 -0500

    feat: add service to create categories

--- src/services/categories.service.ts
+++ src/services/categories.service.ts
@@ -1,13 +1,27 @@
 import axios from "axios";
 
 import { API_BASE_URL, LIST_CATEGORIES } from "../shared/constants";
-import { Category } from "../models";
+import { Category, CreateCategoryPayload } from "../models";
 
 export async function listCategories(): Promise<Category[]> {
   try {
     const result = await axios.get<Category[]>(
       `${API_BASE_URL}${LIST_CATEGORIES}`,
     );
+    return result.data;
+  } catch (e: any) {
+    throw new Error(e);
+  }
+}
+
+export async function createCategory(tag: CreateCategoryPayload): Promise<Category> {
+  const access = localStorage.getItem("access");
+  try {
+    const result = await axios.post(`${API_BASE_URL}${LIST_CATEGORIES}`, tag, {
+      headers: {
+        Authorization: `Bearer ${access}`,
+      },
+    });
     return result.data;
   } catch (e: any) {
     throw new Error(e);
