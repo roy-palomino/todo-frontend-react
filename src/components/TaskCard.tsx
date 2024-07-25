58d3f73d94cc9ea30c0c1523dc8ed67469ed0417
Roy Palomino Rojas <roy100995@gmail.com>
2024-07-25 17:44:28 -0500

    refactor: config time in utc and update layout
    - display date and time in utc
    - change gaps in tag badges
    - justify badges to left

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,23 +1,40 @@
 import { FC } from "react";
 
+import { CalendarIcon } from "@heroicons/react/20/solid";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import "dayjs/locale/es";
+
 import { Task } from "../models";
 import TagBadge from "./TagBadge";
+
+dayjs.extend(utc);
 
 interface Props extends Task {}
 
 const TaskCard: FC<Props> = (task) => {
   return (
-    <div className="flex border border-slate-200 rounded-lg">
-      <div className="py-3 pl-3">
+    <div className="flex border border-slate-200 rounded-lg shadow-md">
+      <div className="py-6 pl-3">
         <input
           className="h-4 w-4 rounded border-gray-300 text-slate-600 focus:ring-transparent"
           type="checkbox"
         />
       </div>
-      <div className="flex flex-col p-3">
-        <h2 className="text-lg font-semibold text-slate-600">{task.name}</h2>
-        <p className="text-sm text-slate-400 font-light">{task.description}</p>
-        <div>
+      <div className="flex flex-col py-6 pl-2">
+        <h2 className="text-lg font-semibold text-slate-600 mb-2">
+          {task.name}
+        </h2>
+        <p className="text-sm text-slate-400 font-light mb-2">
+          {task.description}
+        </p>
+        {task.due_date && (
+          <div className="flex flex-row text-slate-400 font-light text-sm mb-4">
+            <CalendarIcon className="size-5 mr-1" />
+            {dayjs(task.due_date).utc(true).format("DD/MM/YYYY HH:mm a")}
+          </div>
+        )}
+        <div className="flex flex-wrap gap-x-3 gap-y-2">
           {task.tags.map((tag) => (
             <TagBadge key={tag.id} {...tag} />
           ))}
