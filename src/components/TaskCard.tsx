f75e29bad8100be10a5cb4f0b53822e168c8988b
Roy Palomino Rojas <roy100995@gmail.com>
2024-07-25 23:45:09 -0500

    refactor: set a local instance of task prop

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
 
 import { CalendarIcon } from "@heroicons/react/20/solid";
 import dayjs from "dayjs";
@@ -7,40 +7,67 @@ import "dayjs/locale/es";
 
 import { Task } from "../models";
 import TagBadge from "./TagBadge";
+// import { updateCompletedStatus } from "../services/tasks.service.ts";
 
 dayjs.extend(utc);
 
 interface Props extends Task {}
 
-const TaskCard: FC<Props> = (task) => {
+const TaskCard: FC<Props> = (propTask) => {
+  const [task, setTask] = useState<Task | undefined>();
+  const [loading, setLoading] = useState(false);
+
+  //TODO: complete this xd
+
+  // const changeCompleted = async () => {
+  //   if (loading || !task) return;
+  //   try {
+  //     const taskUpdated = await updateCompletedStatus(task.id, !task.done);
+  //     setTask(taskUpdated);
+  //   } catch (e) {
+  //   } finally {
+  //   }
+  // };
+
+  useEffect(() => {
+    setTask(propTask);
+  }, []);
+
   return (
-    <div className="flex border border-slate-200 rounded-lg shadow-md">
-      <div className="py-6 pl-3">
-        <input
-          className="h-4 w-4 rounded border-gray-300 text-slate-600 focus:ring-transparent"
-          type="checkbox"
-        />
-      </div>
-      <div className="flex flex-col py-6 pl-2">
-        <h2 className="text-lg font-semibold text-slate-600 mb-2">
-          {task.name}
-        </h2>
-        <p className="text-sm text-slate-400 font-light mb-2">
-          {task.description}
-        </p>
-        {task.due_date && (
-          <div className="flex flex-row text-slate-400 font-light text-sm mb-4">
-            <CalendarIcon className="size-5 mr-1" />
-            {dayjs(task.due_date).utc(true).format("DD/MM/YYYY HH:mm a")}
+    task && (
+      <div className="flex border border-slate-200 rounded-lg shadow-md">
+        <div className="py-6 pl-3">
+          <input
+            disabled={loading}
+            // onClick={}
+            className="h-4 w-4 mt-1 rounded border-gray-300 text-slate-600 focus:ring-transparent"
+            type="checkbox"
+          />
+        </div>
+        <div className="flex flex-col py-6 px-2">
+          <h2 className="text-lg font-semibold text-slate-600 mb-2">
+            {task.name}
+          </h2>
+          <p className="text-sm text-slate-400 font-light mb-2">
+            {task.description || "No description provided"}
+          </p>
+          {task.due_date && (
+            <div className="flex flex-row text-slate-400 text-xs mb-4 font-semibold my-1">
+              <CalendarIcon className="size-4 mr-1" />
+              <time>
+                {" "}
+                {dayjs(task.due_date).utc(true).format("DD/MM/YYYY HH:mm a")}
+              </time>
+            </div>
+          )}
+          <div className="flex flex-wrap gap-x-3 gap-y-2">
+            {task.tags.map((tag) => (
+              <TagBadge key={tag.id} {...tag} />
+            ))}
           </div>
-        )}
-        <div className="flex flex-wrap gap-x-3 gap-y-2">
-          {task.tags.map((tag) => (
-            <TagBadge key={tag.id} {...tag} />
-          ))}
         </div>
       </div>
-    </div>
+    )
   );
 };
 
