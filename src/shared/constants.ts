682ce0f5f3a8a2fc439e34c743eb3122963dff76
Roy Palomino Rojas <roy100995@gmail.com>
2024-07-27 23:09:16 -0500

    feat: add refresh endpoint

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,5 +1,6 @@
 export const API_BASE_URL = "http://localhost:8000/api/";
 export const LOGIN = "token/";
+export const REFRESH = "token/refresh/";
 export const REGISTER = "register/";
 export const LIST_TASKS = "tasks/";
 export const LIST_TAGS = "tags/";
