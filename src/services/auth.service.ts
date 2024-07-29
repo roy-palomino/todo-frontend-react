fbdf701fd2744412f96bebaa1696b1b0573f7e52
Roy Palomino Rojas <roy100995@gmail.com>
2024-07-28 21:34:57 -0500

    feat: add logout service, post logout to backend

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,12 +1,22 @@
 import axios from "axios";
 
 import { LoginData, RegisterData, LoginResponse } from "../models";
-import { API_BASE_URL, LOGIN, REGISTER, REFRESH } from "../shared/constants";
+import {
+  API_BASE_URL,
+  LOGIN,
+  REGISTER,
+  REFRESH,
+  LOGOUT,
+} from "../shared/constants";
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true,
 });
+
+const getAccessCredential = () => {
+  return localStorage.getItem("access");
+};
 
 export async function login(data: LoginData): Promise<LoginResponse> {
   try {
@@ -29,6 +39,20 @@ export async function refresh(): Promise<LoginResponse> {
   try {
     const result = await axiosInstance.post<LoginResponse>(REFRESH);
     return result.data;
+  } catch (e: any) {
+    location.assign("/logout");
+    throw new Error(e);
+  }
+}
+
+export async function logout(): Promise<void> {
+  let access = getAccessCredential();
+  try {
+    await axiosInstance.post<void>(LOGOUT, null, {
+      headers: {
+        Authorization: `Bearer ${access}`,
+      },
+    });
   } catch (e: any) {
     throw new Error(e);
   }
