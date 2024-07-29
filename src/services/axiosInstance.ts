85e0fbe8d7b201162765bda595db185f6877d89f
Roy Palomino Rojas <roy100995@gmail.com>
2024-07-28 21:30:46 -0500

    feat: configure axios instance to handle only-http refresh token

---
+++ src/services/axiosInstance.ts
@@ -0,0 +1,32 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+
+import { API_BASE_URL } from "../shared/constants";
+import { refresh } from "./auth.service";
+
+const axiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+  withCredentials: true,
+  timeout: 10000,
+});
+
+axiosInstance.interceptors.response.use(
+  (response: AxiosResponse) => {
+    console.log("got response, returning it");
+    return response;
+  },
+  async (error: AxiosError) => {
+    if (error.response?.status === 401) {
+      try {
+        const result = await refresh();
+        localStorage.setItem("access", result.access);
+        return null;
+      } catch (error) {
+        //logout
+        return Promise.reject(error);
+      }
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default axiosInstance;
