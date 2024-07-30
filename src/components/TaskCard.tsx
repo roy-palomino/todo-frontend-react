08f226b866c9c45892fcdf4d4c5d357d819f5dc2
Roy Palomino Rojas <roy100995@gmail.com>
2024-07-29 19:45:22 -0500

    refactor: change behavior on update checked state
    - receive onChange parameter to notify parent on changes
    - use toast to notify user when the task is done or undone
    - add edit button to access details and change them

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,53 +1,62 @@
-import { FC, useState, useEffect } from "react";
+import { FC, useState } from "react";
 
-import { CalendarIcon } from "@heroicons/react/20/solid";
+import { CalendarIcon, PencilSquareIcon } from "@heroicons/react/20/solid";
+import { toast } from "sonner";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import "dayjs/locale/es";
 
 import { Task } from "../models";
 import TagBadge from "./TagBadge";
-// import { updateCompletedStatus } from "../services/tasks.service.ts";
+import CategoryBadge from "./CategoryBadge.tsx";
+import { updateCompletedStatus } from "../services/tasks.service.ts";
 
 dayjs.extend(utc);
 
-interface Props extends Task {}
+interface Props {
+  task: Task;
+  onChecked: () => void;
+}
 
-const TaskCard: FC<Props> = (propTask) => {
-  const [task, setTask] = useState<Task | undefined>();
+const TaskCard: FC<Props> = ({ task, onChecked }) => {
   const [loading, setLoading] = useState(false);
 
-  //TODO: complete this xd
-
-  // const changeCompleted = async () => {
-  //   if (loading || !task) return;
-  //   try {
-  //     const taskUpdated = await updateCompletedStatus(task.id, !task.done);
-  //     setTask(taskUpdated);
-  //   } catch (e) {
-  //   } finally {
-  //   }
-  // };
-
-  useEffect(() => {
-    setTask(propTask);
-  }, []);
+  const changeCompleted = async () => {
+    setLoading(true);
+    if (loading || !task) return;
+    try {
+      const result = await updateCompletedStatus(task.id, !task.done);
+      if (result.done) {
+        toast.success("Task done succesfully");
+      } else {
+        toast.warning("Task undone");
+      }
+      onChecked();
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     task && (
-      <div className="flex border border-slate-200 rounded-lg shadow-md">
-        <div className="py-6 pl-3">
+      <div className="flex border border-slate-300 rounded-lg shadow-md relative">
+        <CategoryBadge category={task.categories[0]} />
+        <div className="py-6 pl-3 mt-1">
           <input
             disabled={loading}
-            // onClick={}
+            checked={task.done}
+            onChange={changeCompleted}
             className="h-4 w-4 mt-1 rounded border-gray-300 text-slate-600 focus:ring-transparent"
             type="checkbox"
           />
         </div>
-        <div className="flex flex-col py-6 px-2">
-          <h2 className="text-lg font-semibold text-slate-600 mb-2">
-            {task.name}
-          </h2>
+        <div className="flex flex-col py-6 px-3 w-full">
+          <div className="flex flex-row justify-between text-slate-600 mt-1 mb-2 items-center">
+            <h2 className="text-lg font-semibold">{task.name}</h2>
+            <PencilSquareIcon className="size-6" />
+          </div>
           <p className="text-sm text-slate-400 font-light mb-2">
             {task.description || "No description provided"}
           </p>
