9f684dd9604b3e0fe87c2237b003b6c3e602e81c
Roy Palomino Rojas <roy100995@gmail.com>
2024-08-01 18:10:43 -0500

    feat: add user and settings interfaces

--- src/models/auth.ts
+++ src/models/auth.ts
@@ -1,4 +1,15 @@
+import { UserSettings } from "./common";
+
 export type AuthInput = {
   username: string;
   password: string;
 };
+
+export interface User {
+  readonly id: number;
+  username: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  settings: UserSettings;
+}
