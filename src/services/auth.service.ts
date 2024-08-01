50954cfbf4c0788af154651505ab94bd9294260d
Roy Palomino Rojas <roy100995@gmail.com>
2024-08-01 18:11:46 -0500

    feat: add self service to get user info

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,26 +1,27 @@
 import axios from "axios";
 
 import { LoginData, RegisterData, LoginResponse } from "../models";
+import useAuth from "../stores/useAuth";
+import { getAccessCredential } from "./service-utils";
 import {
   API_BASE_URL,
   LOGIN,
   REGISTER,
   REFRESH,
   LOGOUT,
+  SELF,
 } from "../shared/constants";
+import { User } from "../models";
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true,
 });
 
-const getAccessCredential = () => {
-  return localStorage.getItem("access");
-};
-
 export async function login(data: LoginData): Promise<LoginResponse> {
   try {
     const result = await axiosInstance.post<LoginResponse>(LOGIN, data);
+    useAuth.setState({ access: result.data.access });
     return result.data;
   } catch (e) {
     throw new Error(`Error when login the user: ${e}`);
@@ -53,6 +54,20 @@ export async function logout(): Promise<void> {
         Authorization: `Bearer ${access}`,
       },
     });
+  } catch (e: any) {
+    throw new Error(e);
+  }
+}
+
+export async function getSelf(): Promise<User> {
+  const access = getAccessCredential();
+  try {
+    const result = await axiosInstance.get(SELF, {
+      headers: {
+        Authorization: `Bearer ${access}`,
+      },
+    });
+    return result.data;
   } catch (e: any) {
     throw new Error(e);
   }
