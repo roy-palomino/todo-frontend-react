ea5178e9594ccdb9215816692b4eaa1f1f949087
Roy Palomino Rojas <roy100995@gmail.com>
2024-08-05 17:38:19 -0500

    feat: add cn function to merge tw classes

--- src/utils/classNames.ts
+++ src/utils/classNames.ts
@@ -1,3 +1,11 @@
+import { clsx, type ClassValue } from "clsx";
+
+import { twMerge } from "tailwind-merge";
+
 export function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
+}
+
+export function cn(...classes: ClassValue[]) {
+  return twMerge(clsx(classes));
 }
