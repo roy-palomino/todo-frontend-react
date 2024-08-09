0871abb322135d5b7094d846b7a96e36f120777f
Roy Palomino Rojas <roy100995@gmail.com>
2024-08-08 21:37:17 -0500

    feat: list tasks organized by dates

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,11 +1,84 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
+
+import dayjs from "dayjs";
+import isToday from "dayjs/plugin/isToday";
+
+import { Task } from "../models";
+import { listTasks } from "../services/tasks.service";
+import TaskCard from "./TaskCard";
+
+dayjs.extend(isToday);
 
 interface Props {
-};
+  hideCompleted: boolean;
+}
 
-const TaskList: FC<Props> = () => {
+interface ClassifiedTasks {
+  [key: string]: Task[];
+}
+
+const TaskList: FC<Props> = ({ hideCompleted = false }) => {
+  const [classifiedTasks, setTasks] = useState<ClassifiedTasks | null>(null);
+
+  function classifyTasks(tasks: Task[]): ClassifiedTasks {
+    let classifiedTasks = tasks.reduce((accumulator: any, current: Task) => {
+      let due_date = dayjs(current.due_date).isToday()
+        ? "Today"
+        : dayjs(current.due_date).format("MMM DD").toString();
+      if (due_date && !accumulator[due_date]) {
+        accumulator[due_date] = [current];
+      } else {
+        accumulator[due_date].push(current);
+      }
+      return accumulator;
+    }, {});
+    return classifiedTasks;
+  }
+
+  function renderUndoneTasks(tasks: Task[]) {
+    return tasks.map(
+      (task) =>
+        !task.done && (
+          <li key={task.id}>
+            <TaskCard onChecked={fetchTasks} task={task} />
+          </li>
+        ),
+    );
+  }
+
+  function renderAllTasks(tasks: Task[]) {
+    return tasks.map((task) => (
+      <li key={task.id}>
+        <TaskCard onChecked={fetchTasks} task={task} />
+      </li>
+    ));
+  }
+
+  async function fetchTasks() {
+    const tasks = await listTasks();
+    setTasks(classifyTasks(tasks));
+  }
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
   return (
-    <div>hola</div>
+    <ul>
+      {!!classifiedTasks &&
+        Object.keys(classifiedTasks).map((due_date: string) => (
+          <li className="hidden has-[li]:block" key={due_date}>
+            <h3 className="text-slate-600 text-center text-xl mt-6 mb-1">
+              {due_date}
+            </h3>
+            <ul className="flex flex-col space-y-4">
+              {hideCompleted
+                ? renderUndoneTasks(classifiedTasks[due_date])
+                : renderAllTasks(classifiedTasks[due_date])}
+            </ul>
+          </li>
+        ))}
+    </ul>
   );
 };
 
