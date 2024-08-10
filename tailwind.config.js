722185b1ac7cf179d38cd1350bf7700879baaa52
Roy Palomino Rojas <roy100995@gmail.com>
2024-08-09 22:14:38 -0500

    feat: add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-1": "#125566",
+        "primary-2": "#106D78",
+        "secondary-1": "#0CA5A3",
+        "secondary-2": "#00BAB8",
+        "secondary-3": "#D07D9B",
+        "accent-1": "#FF0000",
+        "accent-2": "#23A26D",
+        "dark": "#253334",
+      }
+    },
   },
   plugins: [
     require("@tailwindcss/forms"),
