71c4a655224c019eeec2741a5894b989cf252b09
Roy Palomino Rojas <roy100995@gmail.com>
2024-08-23 18:25:25 -0500

    refactor: remove plus icon and display task form on lg

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -7,6 +7,8 @@ import Button from "../components/Button";
 import useAuth from "../stores/useAuth";
 import TaskList from "../components/TaskList";
 
+import TaskForm from "../components/TaskForm";
+
 const Home = () => {
   const settings = useAuth((store) => store.settings);
   const toggleHideTasks = useAuth((store) => store.toggleHideCompleted);
@@ -21,23 +23,29 @@ const Home = () => {
 
   return (
     <Default>
-      <div className="w-full relative bg-transparent">
-        <div className="flex flex-row justify-end mb-4 mt-2">
-          <button
-            onClick={toggleHide}
-            className="text-white text-lg font-light bg-primary-2 px-4 py-1 rounded-lg"
+      <div className="w-full grid grid-cols-1 lg:grid-cols-2 gap-12">
+        <div className="w-full relative bg-transparent">
+          <div className="flex flex-row justify-start mb-4 mt-2">
+            <button
+              onClick={toggleHide}
+              className="text-white text-lg font-light bg-primary-2 px-4 py-1 rounded-lg"
+            >
+              {settings?.hide_completed_tasks ? "Show" : "Hide"} completed tasks
+            </button>
+          </div>
+          <TaskList hideCompleted={!!settings?.hide_completed_tasks} />
+          <Button
+            onClick={() => navigate("/new-task")}
+            rounded={true}
+            className="fixed !shadow-xl bottom-16 md:bottom-40 right-4 bg-accent-1 border-accent-1 lg:hidden"
           >
-            {settings?.hide_completed_tasks ? "Show" : "Hide"} completed tasks
-          </button>
+            <PlusIcon className="size-6 md:size-12" />
+          </Button>
         </div>
-        <TaskList hideCompleted={!!settings?.hide_completed_tasks} />
-        <Button
-          onClick={() => navigate("/new-task")}
-          rounded={true}
-          className="fixed !shadow-xl bottom-16 md:bottom-40 right-4 bg-accent-1 border-accent-1"
-        >
-          <PlusIcon className="size-6 md:size-12" />
-        </Button>
+        <div className="hidden lg:block mt-4">
+          <div className="text-white font-semibold text-xl mb-8">Add a new task</div>
+          <TaskForm />
+        </div>
       </div>
     </Default>
   );
