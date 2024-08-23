67b664fc87ce3d23df4af9a8cf300a3f88adeb9a
Roy Palomino Rojas <roy100995@gmail.com>
2024-08-23 17:52:30 -0500

    refactor: set max-w for main content

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -9,7 +9,7 @@ const DefaultLayout: FC<Props> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-dvh bg-primary-1">
       <AppBar />
-      <div className="container mx-auto flex flex-1 h-max px-5 py-4">{children}</div>
+      <div className="container mx-auto flex flex-1 h-max px-5 py-4 max-w-[1200px]">{children}</div>
     </div>
   );
 };
